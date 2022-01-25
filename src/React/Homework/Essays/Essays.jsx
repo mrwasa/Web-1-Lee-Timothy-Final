985ffa3c95b768dae04ac0d6dcc8797e43c1316f
Timothy Lee <mrwasa@hotmail.com>
2022-01-24 20:32:30 -0800

    Updating Homework

---
+++ src/React/Homework/Essays/Essays.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Essay from './Essay.jsx';
+
+const Essays = () => {
+    return (
+        <EssaysStyled>
+            <h2>Essays</h2>
+            <Essay q='S2. What is the difference between Git, Github and Heroku?' >
+                This is my answer
+            </Essay>
+            <Essay q='S3. Explain the difference between HTML, CSS and JS.' >
+                something else
+            </Essay>
+            <Essay q='S4. What are Single Page Apps (SPAs)? How do they differ from traditional multi-page Web Sites?' >
+                yet another
+            </Essay>
+        </EssaysStyled>
+    )
+}
+
+export default Essays;
+
+const EssaysStyled = styled.div`
+    background-color: white;
+    padding: 20px;
+`;
