8affd1523a4f4fe4ff617f11dabf8661ebe9013b
Timothy Lee <mrwasa@hotmail.com>
2022-01-27 20:14:51 -0800

    Styling Header

--- src/React/Layout/Main.jsx
+++ src/React/Layout/Main.jsx
@@ -37,4 +37,11 @@ export default Main;
 
 const MainStyled = styled.main`
     
+    .inset {
+        border: solid 10px purple;
+        padding: 20px;
+        max-width: 800px;
+        margin: auto;
+    }
+
 `;
