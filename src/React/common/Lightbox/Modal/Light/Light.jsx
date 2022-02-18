ad9ec87874ecca839f8b5f9c8edb8060f4dd0128
Timothy Lee <mrwasa@hotmail.com>
2022-02-17 19:12:09 -0800

    Finishing Lightbox

--- src/React/common/Lightbox/Modal/Light/Light.jsx
+++ src/React/common/Lightbox/Modal/Light/Light.jsx
@@ -5,11 +5,11 @@ import Header from './Header/Header.jsx';
 import Main from './Main.jsx';
 
 
-const Light = ({title, modalContent}) => {
+const Light = ({title, modalContent, closeModal}) => {
 
     return (
         <LightStyled className='Light'>
-            <Header title={ title } />
+            <Header title={ title } closeModal={ closeModal } />
             <Main modalContent={ modalContent } />
         </LightStyled>
     );
