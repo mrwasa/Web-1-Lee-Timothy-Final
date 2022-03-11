69157ebe2a193835bca81c1acb1ef0934ae6ad73
Timothy Lee <mrwasa@hotmail.com>
2022-03-10 23:57:36 -0800

    Add Youtube and Fix errors

---
+++ src/React/UX/Youtube/Youtube.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import YouTube from 'react-youtube';
+
+
+const Youtube = () => {
+
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        },
+    };
+
+    return (
+        <YoutubeStyled className='Youtube'>
+            <YouTube 
+            videoId="9BdtGjoIN4E"
+            id="Video"
+            title="TheTermUX"
+            opts={opts}
+            />
+        </YoutubeStyled>
+    );
+}
+
+export default Youtube;
+
+const YoutubeStyled = styled.div`
+
+`;
